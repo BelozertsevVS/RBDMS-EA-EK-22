e30295007a712fe63687471ec6fd6762d7cf3857
PolinaHryhorieva <159442961+PolinaHryhorieva@users.noreply.github.com>
2024-02-18 19:10:14 +0200

    Hryhorieva_Polina_LW_2
    Hryhorieva_Polina_LW_2

---
+++ LW2/Hryhorieva_Polina_LW_2.sql
@@ -0,0 +1,35 @@
+﻿USE [WebStor];
+GO
+CREATE SCHEMA HryhorievaPolina;    
+GO
+-- Create a simple table to demonstrate data types
+CREATE TABLE HryhorievaPolina.DATATYPES (
+       ID  BIGINT, -- Unique identifier of an entity
+	   AGE TINYINT -- Number of years
+);
+
+INSERT INTO HryhorievaPolina.DATATYPES(ID, AGE)
+VALUES(10000, 34);
+
+INSERT INTO HryhorievaPolina.DATATYPES(ID, AGE)
+VALUES(10001, 32);
+
+-- Implicit data type conversion
+INSERT INTO HryhorievaPolina.DATATYPES(ID, AGE)
+VALUES('10002', 32);
+
+-- Implicit data type conversion
+INSERT INTO HryhorievaPolina.DATATYPES(ID, AGE)
+VALUES('10003', '35');
+
+-- Implicit data type conversion
+INSERT INTO HryhorievaPolina.DATATYPES(ID, AGE)
+VALUES('19700101', '35');
+
+-- Implicit data type conversion IS NOT WORKING!
+INSERT INTO HryhorievaPolina.DATATYPES(ID, AGE)
+VALUES('1970-01-01', '35');
+-- Let's take a look on the data
+SELECT * 
+  FROM HryhorievaPolina.DATATYPES;
+
