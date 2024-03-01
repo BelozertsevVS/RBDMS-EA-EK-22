ad32f3e586380f5fbaa7afb67b171bae198864c6
aliissaa <159949283+aliissaa@users.noreply.github.com>
2024-03-01 10:16:29 +0200

    Create Posunko Alisa №2.sql

---
+++ LW2/Posunko Alisa №2.sql
@@ -0,0 +1,23 @@
+﻿USE [WebStor];
+GO
+CREATE SCHEMA POSUNKO;
+go
+
+create table POSUNKO.DATATYPES (
+id bigint,
+age tinyint
+);
+insert into POSUNKO.DATATYPES(id, age)
+values(10000, 34);
+insert into POSUNKO.DATATYPES(id, age)
+values(10001, 32);
+insert into POSUNKO.DATATYPES(id, age)
+values('10002', 32);
+insert into POSUNKO.DATATYPES(id, age)
+values('10003', '35');
+insert into POSUNKO.DATATYPES(id, age)
+values('19700101', '35');
+insert into POSUNKO.DATATYPES(id, age)
+values('1970-01-01', '35');
+SELECT *
+FROM POSUNKO.DATATYPES;
