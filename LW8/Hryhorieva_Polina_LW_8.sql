fb4eb10f210ea370bad66cae4fadffb0f4b1e720
PolinaHryhorieva <159442961+PolinaHryhorieva@users.noreply.github.com>
2024-04-09 20:51:34 +0300

    Hryhorieva_Polina_LW_8
    Hryhorieva_Polina_LW_8

---
+++ LW8/Hryhorieva_Polina_LW_8.sql
@@ -0,0 +1,26 @@
+﻿USE WebStor;
+--Запит, який поверне список працівників старше 45 років
+SELECT EMPL_NUM, NAME, AGE, TITLE 
+FROM [dbo].[SALESREPS]
+WHERE AGE > 45
+ORDER BY AGE DESC;
+/*
+Запит, який поверне список унікальних комбінацій значень ідентифікатора виробника (MFR) 
+та ідентифікатора товару (PRODUCT). Враховується тільки замовлення за 2008 рік.
+*/
+SELECT DISTINCT MFR, PRODUCT
+FROM [dbo].[ORDERS]
+WHERE ORDER_DATE BETWEEN '20080101' AND '20081231';
+/*
+Запит, який поверне ідентифікатор працівника ([REP]) з найбільшою кількістю проведених замовлень.
+Враховується можливість того, що одразу кілька працівників можуть мати однакову кількість проведених замовлень.
+Враховується тільки замовлення за 2008 рік.
+*/          
+SELECT TOP 1 WITH TIES 
+       [REP]
+	  ,[QTY]
+      ,COUNT (*) AS OrderCount
+FROM [dbo].[ORDERS]
+WHERE ORDER_DATE BETWEEN '20080101' AND '20081231'
+GROUP BY [REP], [QTY]
+ORDER BY [QTY] DESC;
