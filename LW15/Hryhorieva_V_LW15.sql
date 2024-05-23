3952460d75fdbca70eb907521fef78498f03cadd
Victoria732 <grigorevaviktoria732@gmail.com>
2024-05-23 18:20:14 +0300

    Create Hryhorieva_V_LW15.sql

---
+++ LW15/Hryhorieva_V_LW15.sql
@@ -0,0 +1,40 @@
+﻿/*Задача 1
+Напишіть запит, який поверне ідентифікатор клієнта (CUST_NUM) з найбільшим кредитним лімітом (CREDIT_LIMIT).
+Враховуйте ймовірність того, що одразу кілька клієнтів можуть мати однаковий кредитний ліміт.
+- Використовується таблиця [dbo].[CUSTOMERS]
+- Не використовуйте оператор with ties
+- Результативний набір даних містить: Ідентифікатор клієнта*/
+
+SELECT*FROM CUSTOMERS
+SELECT CUST_NUM FROM CUSTOMERS 
+WHERE CREDIT_LIMIT = 
+(SELECT MAX([CREDIT_LIMIT]) FROM CUSTOMERS)
+
+/*Задача 2
+Напишіть запит, що повертає список замовлень клієнта з найбільшим кредитним лімітом (CREDIT_LIMIT).
+Враховуйте ймовірність того, що одразу кілька клієнтів можуть мати однаковий кредитний ліміт.
+- Використовується таблиці [dbo].[CUSTOMERS], [dbo].[ORDERS]
+- Не використовуйте оператор with ties
+- Результативний набір даних містить: Ідентифікатор клієнта*/
+
+SELECT TOP 1 C.[CUST_NUM] FROM [dbo].[ORDERS] O 
+JOIN (SELECT C.[CUST_NUM], C.[CREDIT_LIMIT]
+FROM [dbo].[CUSTOMERS] C
+WHERE C.[CREDIT_LIMIT] = (SELECT MAX([CREDIT_LIMIT])
+FROM [dbo].[CUSTOMERS])) 
+c ON O.CUST = C.CUST_NUM
+
+/*Задача 3
+Напишіть запит, що повертає найновіше (за датою проведення ORDER_DATE) замовлення серед списку замовлень клієнта з найбільшим кредитним лімітом (CREDIT_LIMIT).
+Враховуйте ймовірність того, що одразу кілька замовлень можуть мати однакову дату проведення.
+Враховуйте ймовірність того, що одразу кілька клієнтів можуть мати однаковий кредитний ліміт.
+- Використовується таблиці [dbo].[CUSTOMERS], [dbo].[ORDERS]
+- Не використовуйте оператор with ties
+- Результативний набір даних містить: Ідентифікатор клієнта*/
+
+SELECT TOP 1 C.[CUST_NUM] 
+FROM [dbo].[CUSTOMERS] C
+JOIN [dbo].[ORDERS] O 
+ON C.[CUST_NUM] = O.CUST
+WHERE C.[CREDIT_LIMIT] = (SELECT MAX([CREDIT_LIMIT])
+FROM [dbo].[CUSTOMERS])
